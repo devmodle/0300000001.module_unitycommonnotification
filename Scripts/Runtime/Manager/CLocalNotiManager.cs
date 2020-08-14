5ba3afc54e348e736bdd06a138d75b3ce89b11ee
sd.lee <sd.lee@ninetap.com>
2020-08-14 19:41:02 +0900

    [이상동] 유니티 패키지 설정 추가

---
+++ Scripts/Runtime/Manager/CLocalNotiManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if LOCAL_NOTI_ENABLE
+//! 로컬 알림 관리자
+public class CLocalNotiManager : CSingleton<CLocalNotiManager> {
+	#region 변수
+	private System.Action<CLocalNotiManager, bool> m_oInitCallback = null;
+	#endregion			// 변수
+
+	#region 프로퍼티
+	public bool IsInit { get; private set; } = false;
+	#endregion			// 프로퍼티
+
+	#region 함수
+	//! 초기화
+	public virtual void Init(System.Action<CLocalNotiManager, bool> a_oCallback) {
+		CFunc.ShowLog("CLocalNotiManager.Init", KCDefine.B_LOG_COLOR_PLUGIN);
+
+		if(!this.IsInit && (CAccess.IsEditorPlatform() || CAccess.IsMobilePlatform())) {
+			this.IsInit = true;
+		}
+
+		a_oCallback?.Invoke(this, this.IsInit);
+	}
+	#endregion			// 함수
+}
+#endif			// #if LOCAL_NOTI_ENABLE
