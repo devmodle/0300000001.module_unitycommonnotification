a4e5cb2110c200e75f1d5efd5d3d4dd7ac8cf31c
sd.lee <sd.lee@ninetap.com>
2020-12-14 12:21:51 +0900

    [이상동] 코드 정렬 방식 변경

--- Scripts/Runtime/Manager/CNotiManager.cs
+++ Scripts/Runtime/Manager/CNotiManager.cs
@@ -119,9 +119,7 @@ public class CNotiManager : CSingleton<CNotiManager> {
 			// 반복 모드 일 경우
 			if(a_stNotiInfo.m_bIsRepeat) {
 				oNoti.RepeatInterval = new System.TimeSpan(KCDefine.B_VALUE_INT_1, 
-					KCDefine.B_VALUE_INT_0, 
-					KCDefine.B_VALUE_INT_0, 
-					KCDefine.B_VALUE_INT_0);
+					KCDefine.B_VALUE_INT_0, KCDefine.B_VALUE_INT_0, KCDefine.B_VALUE_INT_0);
 			}
 
 			int nID = this.MakeNotiID(a_oKey);
