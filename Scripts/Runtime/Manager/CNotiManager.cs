0a43a53fd336ad10627af4e953fb6228d626d7ee
dante <are2341@nate.com>
2022-05-11 23:31:42 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CNotiManager.cs
+++ Scripts/Runtime/Manager/CNotiManager.cs
@@ -61,7 +61,7 @@ public partial class CNotiManager : CSingleton<CNotiManager> {
 #if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
 		// 초기화 되었을 경우
 		if(this.IsInit) {
-			a_stParams.m_oCallbackDict?.GetValueOrDefault(ECallback.INIT)?.Invoke(this, true);
+			a_stParams.m_oCallbackDict?.GetValueOrDefault(ECallback.INIT)?.Invoke(this, this.IsInit);
 		} else {
 			m_stParams = a_stParams;
 
